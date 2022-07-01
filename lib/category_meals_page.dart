17d3c8c522742e65a5d3e844d6ef2e89ac59b6d6
森田陸斗 <mrmrt1121@gmail.com>
2022-07-01 22:43:22 +0900

    カテゴリを含んでいるかを調査する処理の追加

--- lib/category_meals_page.dart
+++ lib/category_meals_page.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
+import './models/meal.dart';
+import './dummy_data.dart';
+
 class CategoryMealsPage extends StatelessWidget {
   const CategoryMealsPage(
       {Key? key, required this.categoryId, required this.categoryTitle})
       : super(key: key);
-      
+
   final String categoryId;
   final String categoryTitle;
 
   @override
   Widget build(BuildContext context) {
+    final List<Meal> categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text(
-          categoryId,
-          style: Theme.of(context).textTheme.bodyLarge,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                categoryMeals[index].title,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            );
+          },
+          itemCount: categoryMeals.length,
         ),
       ),
     );
