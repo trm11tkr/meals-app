5edabc82a6e7a47e8df70b0c1bb091a28c1066ef
森田陸斗 <mrmrt1121@gmail.com>
2022-07-01 23:41:00 +0900

    MealItemにcomplexityを表示

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -8,6 +8,19 @@ class MealItem extends StatelessWidget {
   final Meal meal;
   void selectMeal() {}
 
+  String get complexityText {
+    switch (meal.complexity) {
+      case Complexity.simple:
+        return 'Simple';
+      case Complexity.challenging:
+        return 'Challenging';
+      case Complexity.hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -18,58 +31,81 @@ class MealItem extends StatelessWidget {
         ),
         elevation: 4,
         margin: const EdgeInsets.all(10),
-        child: Column(children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-                child: Image.network(
-                  meal.imageUrl,
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                bottom: 20,
-                right: 10,
-                child: Container(
-                  width: 300,
-                  color: Colors.black54,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                  child: Text(
-                    meal.title,
-                    style: const TextStyle(
-                      fontSize: 26,
-                      color: Colors.white,
-                    ),
-                    softWrap: true,
-                    overflow: TextOverflow.fade,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(children: [
-              Icon(
-                Icons.schedule,
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.schedule,
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        '${meal.duration} min',
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.work,
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        complexityText,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(
-                width: 6,
-              ),
-              Text(
-                '${meal.duration} min',
-                style: Theme.of(context).textTheme.labelMedium,
-              ),
-            ]),
-          )
-        ]),
+            ),
+          ],
+        ),
       ),
     );
   }
