3f9ad1b8185af891736a63c0a943a9be5f74ca07
森田陸斗 <mrmrt1121@gmail.com>
2022-07-01 16:30:55 +0900

    カテゴリータップ時にページ遷移

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_page.dart';
+
 class CategoryItem extends StatelessWidget {
   const CategoryItem({Key? key, required this.title, required this.color})
       : super(key: key);
   final String title;
   final Color color;
 
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoryPage();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.5),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.5),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.bodyLarge,
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
       ),
     );
   }
