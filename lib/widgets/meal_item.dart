2790638c86ba6cdebab912cdc913cdaa7304fdd1
森田陸斗 <mrmrt1121@gmail.com>
2022-07-01 23:20:23 +0900

    MealItemのtitle表示を修正

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,7 +17,7 @@ class MealItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         elevation: 4,
-        margin:const EdgeInsets.all(4),
+        margin: const EdgeInsets.all(10),
         child: Column(children: [
           Stack(
             children: [
@@ -26,7 +26,31 @@ class MealItem extends StatelessWidget {
                   topLeft: Radius.circular(15),
                   topRight: Radius.circular(15),
                 ),
-                child: Image.network(meal.imageUrl, height: 250, width: double.infinity, fit: BoxFit.cover,),
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 300,
+                  color: Colors.black54,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    meal.title,
+                    style: const TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
               )
             ],
           )
