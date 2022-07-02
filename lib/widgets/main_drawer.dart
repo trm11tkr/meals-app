00c9e5b70739734006e1487c28b7cf8729d73790
森田陸斗 <mrmrt1121@gmail.com>
2022-07-02 17:38:10 +0900

    drawerの簡易作成（タップ処理なし）

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key? key}) : super(key: key);
+
+  Widget _buildListTile(String title, IconData iconData) {
+    return ListTile(
+      leading: Icon(iconData, size: 26),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontFamily: 'KaiseiOpti',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    return Drawer(
+      backgroundColor: const Color.fromRGBO(255, 254, 229, 1),
+      child: Column(
+        children: [
+          Container(
+            height: mediaQuery.size.height * 0.2,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'KaiseiOpti',
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          _buildListTile('Meals', Icons.restaurant),
+          _buildListTile('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
