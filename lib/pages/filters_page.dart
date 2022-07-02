2d704e9f5657163a87496b1fe462ecae528de1b6
森田陸斗 <mrmrt1121@gmail.com>
2022-07-02 18:48:15 +0900

    FilterPageをStatefulWidgetに変更

--- lib/pages/filters_page.dart
+++ lib/pages/filters_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FilterPage extends StatelessWidget {
+class FilterPage extends StatefulWidget {
   const FilterPage({Key? key}) : super(key: key);
 
+  @override
+  State<FilterPage> createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
