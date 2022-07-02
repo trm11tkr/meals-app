27f98657bd4dda6c6b2736daa4d8fcceccaa31d3
森田陸斗 <mrmrt1121@gmail.com>
2022-07-02 16:15:46 +0900

    タブページの簡易作成

---
+++ lib/pages/tabs_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+import './categories_page.dart';
+
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabPage> createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Meals'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Category',
+              ),
+              Tab(
+                icon: Icon(Icons.favorite),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          CategoriesPage(),
+        ]),
+      ),
+    );
+  }
+}
